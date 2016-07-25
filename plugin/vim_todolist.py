19ac79d2794fbe1ee12e350ea694edc5bcf1d397
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2016-07-25 03:09:57 +0300

    0: toggling presence of checkmakr

--- plugin/vim_todolist.py
+++ plugin/vim_todolist.py
@@ -1,31 +1,45 @@
 import vim
 import re
 
-todo_item_regex = re.compile("(^\s*\[)(.)(\].*$)")
+todo_item_regex = re.compile("(^\s*)(\[.\])(\s*)(.*$)")
 brackets_string = "[ ]"
 
-def append_todo_line():
+def append():
     cb = vim.current.buffer
     (row, col) = vim.current.window.cursor
     cb.append(brackets_string, row)
 
-def promote_to_todo_item():
+# TODO: detect the todo level
+def promote():
     line = vim.current.line
     if is_todo_item(line):
         return 
     vim.current.line = brackets_string + " " + vim.current.line
 
+def demote():
+    line = vim.current.line
+    if not is_todo_item(line):
+        return 
+    vim.current.line = re.sub(todo_item_regex, r'\g<1>\g<4>', line)
+
+def toggle_item():
+    line = vim.current.line
+    if not is_todo_item(line):
+        promote()
+    else:
+        demote()
+
 def mark_as_done():
     line = vim.current.line
     if not is_todo_item(line):
         return
-    vim.current.line = re.sub(todo_item_regex, r'\g<1>x\g<3>', line)
+    vim.current.line = re.sub(todo_item_regex, r'\g<1>[x]\g<3>\g<4>', line)
 
 def mark_as_undone():
     line = vim.current.line
     if not is_todo_item(line):
         return
-    vim.current.line = re.sub(todo_item_regex, r'\g<1> \g<3>', line)
+    vim.current.line = re.sub(todo_item_regex, r'\g<1>[ ]\g<3>\g<4>', line)
 
 def is_todo_item(line):
     return todo_item_regex.match(line)
@@ -36,9 +50,9 @@ def is_done(line):
     match = re.match(todo_item_regex, line)
     if match == None:
         return
-    return not re.match(r'\s', match.group(2))
+    return not re.match(r'\[\s\]', match.group(2))
 
-def toggle():
+def toggle_mark():
     line = vim.current.line
 
     if not is_todo_item(line):
--- plugin/vim_todolist.vim
+++ plugin/vim_todolist.vim
@@ -2,21 +2,23 @@ python3 import sys
 python3 import vim
 python3 sys.path.append(vim.eval('expand("<sfile>:h")'))
 
-function! Vim_TodoList_Promote()
-python3 << ENDOFPYTHON
-from vim_todolist import promote_to_todo_item
-promote_to_todo_item()
-ENDOFPYTHON
-endfunction
-
 
 function! Vim_TodoList_Toggle()
 python3 << ENDOFPYTHON
-from vim_todolist import toggle
-toggle()
+from vim_todolist import toggle_item
+toggle_item()
 ENDOFPYTHON
 endfunction
 
-noremap <leader>tt :call Vim_TodoList_Promote()<cr>
-noremap <leader><space> :call Vim_TodoList_Toggle()<cr>
+
+function! Vim_TodoList_ToggleMark()
+python3 << ENDOFPYTHON
+from vim_todolist import toggle_mark
+toggle_mark()
+ENDOFPYTHON
+endfunction
+
+
+noremap <leader>tt :call Vim_TodoList_Toggle()<cr>
+noremap <leader><space> :call Vim_TodoList_ToggleMark()<cr>
 
